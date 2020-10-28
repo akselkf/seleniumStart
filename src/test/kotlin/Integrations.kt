7e9d9253292799a54afc9216f9d4fd427f3ddbd7
akselkf <akselkf@gmail.com>
2020-10-28 11:44:19 +0100

    kan logge inn i PRO

--- src/test/kotlin/Integrations.kt
+++ src/test/kotlin/Integrations.kt
@@ -3,9 +3,14 @@ import org.junit.Test
 internal class Integrations {
 
     @Test
-    fun hello() {
+    fun login_pro() {
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe")
-        driver.get("http://vg.no")
+        driver.get("http://192.168.11.153:20000")
+        driver.findElementById("login-username").sendKeys("admin")
+        driver.findElementById("login-password").sendKeys("admin")
+        driver.findElementById("login-submit").click()
+
+        Thread.sleep(3000)
         driver.close()
     }
 }
