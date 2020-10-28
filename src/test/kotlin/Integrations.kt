26d837b2a5747465a75dd9f17f10581d37e262a4
aksfag <aksfag@pasientreiser.no>
2020-10-28 11:52:51 +0100

    teststrukturstart

--- src/test/kotlin/Integrations.kt
+++ src/test/kotlin/Integrations.kt
@@ -1,16 +1,27 @@
+import org.junit.Assert
 import org.junit.Test
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import java.util.concurrent.TimeUnit
 
 internal class Integrations {
 
+    @BeforeEach
+    fun setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe")
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+    }
+
+    @AfterEach
+    fun teardown() {
+        driver.close()
+    }
+
     @Test
     fun login_pro() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe")
         driver.get("http://192.168.11.153:20000")
         driver.findElementById("login-username").sendKeys("admin")
         driver.findElementById("login-password").sendKeys("admin")
         driver.findElementById("login-submit").click()
-
-        Thread.sleep(3000)
-        driver.close()
     }
 }
