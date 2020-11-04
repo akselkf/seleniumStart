e3c072f9c3ee5b9c5cc2555578a6d47b0799c821
aksfag <aksfag@pasientreiser.no>
2020-11-04 10:16:26 +0100

    fix

--- src/test/kotlin/oppgaver/OppgaverLosning.kt
+++ src/test/kotlin/oppgaver/OppgaverLosning.kt
@@ -8,6 +8,8 @@ import no.pasientreiser.pro.tester.proLink
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.openqa.selenium.Keys
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import java.util.concurrent.TimeUnit
 
@@ -29,7 +31,7 @@ class OppgaverLosning : TestBase() {
 
     /**
      * Oppgave 2
-     * Lag en test som tilknytter en sak
+     * Lag en test som tilknytter en sak i PRO
      */
     @Test
     fun `kan tilknytte og hente neste sak løsningsforslag`() {
@@ -53,6 +55,36 @@ class OppgaverLosning : TestBase() {
      */
     @Test
     fun `Kan markere bilag med dato, type og beløp løsningsforslag`() {
+        driver.get(proLink)
+        driver.findElementById("oversikt-kategorisering").click()
+
+        val nr = 1
+        val fullXpathStart = "/html/body/div[4]/div/merkebilag/div[1]/div[2]/div[$nr]/div"
+        val bilagstype = driver.findElementByXPath("$fullXpathStart/div/div/div/input")
+        val datofelt = driver.findElementByXPath("$fullXpathStart/input[1]")
+        val belopfelt = driver.findElementByXPath("$fullXpathStart/input[2]")
+
+        clearSendReturnTab(datofelt, "0101")
+        clearSendReturnTab(bilagstype, "opp")
+        clearSendReturnTab(belopfelt, "140")
+    }
+
+
+
+    fun clearSendReturnTab(field: WebElement, keysToSend: String) {
+        field.clear()
+        field.sendKeys(keysToSend)
+        field.sendKeys(Keys.RETURN)
+        field.sendKeys(Keys.TAB)
+    }
+
+
+    /**
+     * Oppgave 3.1
+     *  Lag en test som markerer bilag på en sak med bruk av page-objectet MerkeBilag
+     */
+    @Test
+    fun `Kan markere bilag med dato, type og beløp med pageobject løsningsforslag`() {
         val digitaliserer = MerkeBilag(driver)
         digitaliserer.tilMerkeBilag()
         digitaliserer.markerBilag(1, "0101", "opp")
@@ -60,8 +92,6 @@ class OppgaverLosning : TestBase() {
         digitaliserer.markerBilag(3, "0101", "park", "88")
 
         hentNesteOgAssert(digitaliserer)
-
-
     }
 
 
